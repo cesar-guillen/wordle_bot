ec1f575a8fe58bc39359438be92eb17bb7882393
cesar-guillen <guillencesar871@gmail.com>
2024-11-25 11:10:27 +0100

    added day functions files for ease of use

--- utils.py
+++ utils.py
@@ -1,30 +1,10 @@
-import time
-import pytz
 from database import *
-from datetime import time ,datetime
+from datetime import datetime
+from day_functions import *
 import math 
 
-SEASON_DURATON = 28
-SEASON_START_DATE = "28/10/2024"
-WORDLE_0 = "19/6/2021"
-CONNECTIONS_0 = "11/6/2023"
 CHANNEL_ID = 944387424426029056
 #CHANNEL_ID = 1303707032842403854
-
-def day_difference(first_date, second_date):
-    day_difference = (first_date - second_date).days
-    return day_difference
-
-def calculate_season_day(today, season_start_date): 
-    return (day_difference(today,season_start_date) % SEASON_DURATON) + 1
-
-season_start_date = datetime.strptime(SEASON_START_DATE, "%d/%m/%Y")
-today = datetime.now(pytz.utc)
-cet = pytz.timezone("Europe/Berlin")
-today_cet = today.astimezone(cet)
-season_start_date_cet = cet.localize(season_start_date)
-current_season_day = calculate_season_day(today_cet, season_start_date_cet)
-new_day = time(hour=23, minute=0)  #this uses utc time 
 
 def calculate_connections_distribution(username_to_check):
     distribution = []
