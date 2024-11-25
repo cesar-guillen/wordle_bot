3a9bba77105117b5839bea80d1b9a0539c9fee6b
cesar-guillen <guillencesar871@gmail.com>
2024-11-25 10:38:38 +0100

    cleaned up and fixed bug

---
+++ database.py
@@ -0,0 +1,79 @@
+import sqlite3
+import math
+from datetime import datetime
+from utils import *
+#database
+database = sqlite3.connect('bot_db')
+cursor = database.cursor
+database.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+        Username TEXT NOT NULL UNIQUE
+    )
+''')
+
+database.execute('''
+    CREATE TABLE IF NOT EXISTS WordleScores (
+        ScoreID INTEGER PRIMARY KEY AUTOINCREMENT,
+        UserID INTEGER,
+        WordleID INTEGER,
+        Guesses INTEGER NOT NULL,
+        FOREIGN KEY(UserID) REFERENCES Users(UserID)
+    )
+''')
+
+database.execute('''
+    CREATE TABLE IF NOT EXISTS ConnectionsScores (
+        ScoreID INTEGER PRIMARY KEY AUTOINCREMENT,
+        UserID INTEGER,
+        ConnectionsID INTEGER,
+        Score INTEGER NOT NULL,
+        FOREIGN KEY(UserID) REFERENCES Users(UserID)
+    )
+''')
+
+database.execute('''
+    CREATE TABLE IF NOT EXISTS SeasonResults (
+        SeasonScoreID INTEGER PRIMARY KEY AUTOINCREMENT,
+        UserID INTEGER,
+        Season INTEGER,
+        Position INTEGER,
+        FOREIGN KEY(UserID) REFERENCES Users(UserID)
+    )
+    ''')
+
+database.commit()
+
+def get_all_users():
+    query = database.execute('SELECT Username FROM Users').fetchall()
+    return query
+
+def add_user_if_not_exists(username):
+    database.execute('''
+        INSERT OR IGNORE INTO Users (Username) VALUES (?)
+    ''', (username,))
+    database.commit() 
+
+def put_connections(username, connections_id, correct):
+    database.execute('INSERT INTO ConnectionsScores (UserID, ConnectionsID, Score) VALUES (?, ?, ?)', 
+                   (username, connections_id, correct))
+    database.commit()
+
+def put_wordle(username, wordle_id, correct):
+    database.execute('INSERT INTO WordleScores (UserID, WordleID, Guesses) VALUES (?, ?, ?)', 
+                   (username, wordle_id, correct))
+    database.commit()
+    
+def put_season_results(rank_list):
+    global season_start_date
+    today = datetime.today()
+    season = int(math.ceil(day_difference(today, season_start_date) / 7) / 4)
+    users = []
+    rank = []
+    for idx, (user, score) in enumerate(rank_list, 1):
+        users.append(user)
+        rank.append(idx)
+    for i in range(len(users)):
+        database.execute('INSERT INTO SeasonResults (UserID, Season, Position) VALUES (?,?,?)',
+                         (users[i],season,rank[i]))
+        database.commit()
---
+++ utils.py
@@ -0,0 +1,141 @@
+import time
+import pytz
+from database import *
+from datetime import time ,datetime
+import math 
+
+SEASON_DURATON = 28
+SEASON_START_DATE = "28/10/2024"
+WORDLE_0 = "19/6/2021"
+CONNECTIONS_0 = "11/6/2023"
+#CHANNEL_ID = 944387424426029056
+CHANNEL_ID = 1303707032842403854
+
+def day_difference(first_date, second_date):
+    day_difference = (first_date - second_date).days
+    return day_difference
+
+def calculate_season_day(today, season_start_date): 
+    return (day_difference(today,season_start_date) % SEASON_DURATON) + 1
+
+season_start_date = datetime.strptime(SEASON_START_DATE, "%d/%m/%Y")
+today = datetime.now(pytz.utc)
+cet = pytz.timezone("Europe/Berlin")
+today_cet = today.astimezone(cet)
+season_start_date_cet = cet.localize(season_start_date)
+current_season_day = calculate_season_day(today_cet, season_start_date_cet)
+new_day = time(hour=23, minute=0)  #this uses utc time 
+
+def calculate_connections_distribution(username_to_check):
+    distribution = []
+    query = database.execute('SELECT COUNT(*) FROM ConnectionsScores WHERE UserID = ?', (username_to_check,))
+    total_count = query.fetchone()[0]
+    for i in range(5):
+        if i == 3:
+            continue
+        query = database.execute('SELECT COUNT(*) FROM ConnectionsScores WHERE Score = ? AND UserID = ?', (i, username_to_check))
+        count = query.fetchone()[0]
+        percentage = (count / total_count) * 100
+        num_dashes = math.ceil((count / total_count) * 40)
+        if num_dashes == 0:
+            num_dashes = 1
+        distribution_line = f'{i} ' + '-' * num_dashes + f'> {percentage:.2f}%'
+        distribution.append(distribution_line)
+
+    formatted_distribution = '\n'.join(distribution)
+    return formatted_distribution
+
+def calculate_wordle_distribution(username_to_check):
+    distribution = []
+    query = database.execute('SELECT COUNT(*) FROM WordleScores WHERE UserID = ?', (username_to_check,))
+    total_count = query.fetchone()[0]
+
+    for i in range(7):
+        score_to_check = f'{i + 1}/6'
+        query = database.execute('SELECT COUNT(*) FROM WordleScores WHERE Guesses = ? AND UserID = ?', (score_to_check, username_to_check))
+        count = query.fetchone()[0]
+        percentage = (count / total_count) * 100
+        num_dashes = math.ceil((count / total_count) * 50)
+        if num_dashes == 0:
+            num_dashes = 1
+        if i == 6:
+            score_to_check = 'X/6'
+        distribution_line = f'{score_to_check} ' + '-' * num_dashes + f'> {percentage:.2f}%'
+        distribution.append(distribution_line)
+
+    formatted_distribution = '\n'.join(distribution)
+    return formatted_distribution
+    
+def get_wordle_leaderboard(days):
+    users_list = get_all_users()
+    wordle_leaderboard_list = []
+    for users in users_list:
+        user = users[0]
+        user_wordle_average = calculate_average_wordle_guesses(user,days)
+        wordle_leaderboard_list.append((user,user_wordle_average))
+    wordle_leaderboard_list.sort(key=lambda x: x[1])
+    return wordle_leaderboard_list
+
+def get_connections_leaderboard(days):
+    users_list = get_all_users()
+    connections_leaderboard_list = []
+    for users in users_list:
+        user = users[0]
+        user_connections_average = calculate_average_connections_guesses(user,days)
+        connections_leaderboard_list.append((user,user_connections_average))
+    connections_leaderboard_list.sort(key=lambda x: x[1], reverse=True)
+    return connections_leaderboard_list
+
+def get_all_leaderboard(days):
+    connections_leaderboard_list = get_connections_leaderboard(days)
+    wordle_leaderboard_list = get_wordle_leaderboard(days)
+    overall_leaderboard_list = []
+    for i, w_user in enumerate(wordle_leaderboard_list):
+        if w_user[1] == 0.0:
+            w_user = list(w_user) 
+            w_user[1] = 7         
+            wordle_leaderboard_list[i] = tuple(w_user) 
+    for c_user in connections_leaderboard_list:
+        for w_user in wordle_leaderboard_list:
+            if c_user[0] == w_user[0]:
+                overall_leaderboard_list.append((c_user[0],(7.0-w_user[1])+c_user[1]))
+    overall_leaderboard_list.sort(key=lambda x: x[1], reverse=True)
+    return overall_leaderboard_list
+
+def calculate_start_id(days):
+    WORDLE_0 = "19/6/2021"
+    start_date_w = datetime.strptime(WORDLE_0, "%d/%m/%Y")
+    start_date_c = datetime.strptime(CONNECTIONS_0, "%d/%m/%Y")
+    today_date = datetime.today()
+    start_id_w = day_difference(today_date, start_date_w) - days
+    start_id_c = day_difference(today_date, start_date_c) - days
+    return [start_id_w, start_id_c]
+
+def calculate_average_wordle_guesses(username_to_check, days):
+    query = database.execute('SELECT Guesses FROM WordleScores WHERE UserID = ? AND WordleID > ?', (username_to_check,calculate_start_id(days)[0]))
+    guesses_db = query.fetchall() 
+    guesses = []
+    for row in guesses_db:
+        guess_str = row[0]
+        guess = int(guess_str.split('/')[0])
+        guesses.append(guess)
+    total_guesses = sum(guesses) 
+    try:
+        average_guesses = total_guesses / len(guesses)
+    except:
+        average_guesses = 0
+
+    return average_guesses
+
+def calculate_average_connections_guesses(username_to_check, days):
+    query = database.execute('SELECT Score FROM ConnectionsScores WHERE UserID = ? AND ConnectionsID > ?', (username_to_check,calculate_start_id(days)[1]))
+    scores_db = query.fetchall() 
+    scores = []
+    for row in scores_db:
+        scores.append(row[0])
+    total_score = sum(scores) 
+    try:
+        averege_score = total_score / len(scores)
+    except:
+        averege_score = 0
+    return averege_score
