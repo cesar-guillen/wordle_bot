4f2841eb4d1a523080c1bd93789904fff478fbc9
cesar-guillen <cesar@PC1>
2025-04-04 11:15:16 +0200

    tmp fix for daylight savings hour change

--- day_functions.py
+++ day_functions.py
@@ -13,7 +13,7 @@ def day_difference(first_date, second_date):
 def calculate_season_day(today, season_start_date): 
     return (day_difference(today,season_start_date) % SEASON_DURATON) + 1
 
-new_day = time(hour=23, minute=00)  #this uses utc times
+new_day = time(hour=22, minute=00)  #this uses utc times
 season_start_date = datetime.strptime(SEASON_START_DATE, "%d/%m/%Y")
 cet = pytz.timezone("Europe/Berlin")
 
@@ -25,4 +25,3 @@ def find_cet_day():
 today_cet = find_cet_day() # server is in utc timezone, changing it to CET
 season_start_date_cet = cet.localize(season_start_date)
 current_season_day = calculate_season_day(today_cet, season_start_date_cet) # get calculated at run time but gets updated in the disc loop
-
